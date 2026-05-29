e17e94c650ee93ff9a841eb457547daa95693b5c
rajax07 <kumar.rajavishvakarma@gmail.com>
2026-05-29 17:52:50 +0530

    solved problem

--- Assignment/Week-02/js-basics/index.js
+++ Assignment/Week-02/js-basics/index.js
@@ -1,16 +1,28 @@
 // 1.
-const expense = {food: [10, 20, 30], travel: [5, 15], bills: [40, 60]};
-const ans = {};
+// const expense = {food: [10, 20, 30], travel: [5, 15], bills: [40, 60]};
+// const ans = {};
 
-for(let [keys, vlaues] of Object.entries(expense)){
-    ans[keys] = vlaues.reduce((acc, curr) => acc + curr, 0);
-}
-console.log(ans);
+// for(let [keys, vlaues] of Object.entries(expense)){
+//     ans[keys] = vlaues.reduce((acc, curr) => acc + curr, 0);
+// }
+// console.log(ans);
 // output : { food: 60, travel: 20, bills: 100 }
 
 
 // 2.
+const words = ["apple", "banana", "apple", "orange", "banana", "apple"];
 
-const arrWord = ["apple", "banana", "apple", "orange", "banana", "apple"]
+const count = {};
 
-const ans = {};
+for (let i = 0; i < words.length; i++) {
+  const word = words[i];
+
+  if (count[word]) {
+    count[word]++;
+  } else {
+    count[word] = 1;
+  }
+}
+
+console.log(count);
+
--- WebDev/class-3.1/reduce.js
+++ WebDev/class-3.1/reduce.js
@@ -3,8 +3,22 @@ const arr = [10, 2, 4, 8];
 
 const total = arr.reduce((acc, curr) => {
     return acc + curr;
-},0);
+},0); // 0 is intial value passed to accumulator 
+// accumulator works: acc = acc + curr
 console.log(total);
+
+
+// without reduce or traditionally
+
+function findSum(arr){
+    let sum = 0;
+    for(let i = 0; i<arr.length; i++){
+        sum+= arr[i];
+    }
+    return sum;
+}
+console.log(findSum(arr));
+
 
 
 
