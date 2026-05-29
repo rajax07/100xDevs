ba174f6fe11ed9d4d2682284028a7d35e43b3e8e
rajax07 <kumar.rajavishvakarma@gmail.com>
2026-05-29 21:23:36 +0530

    connected to postgress

--- WebDev/class-13/index.js
+++ WebDev/class-13/index.js
@@ -1,9 +1,25 @@
 const express = require("express");
+const { Pool } = require('pg')
+
+const pool = new Pool({
+    connectionString: ""
+})
+
 const app = express();
 
 app.use(express.json());
 
-app.post("/signup", (req, res) => {
+app.post("/signup",async (req, res) => {
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    await pool.query(`INSERT INTO users (username, email, password) VALUES ('${username}', '${email}', '${password}')`)
+
+    res.json({
+
+        message:"Signup done"
+    })
 
 });
 
